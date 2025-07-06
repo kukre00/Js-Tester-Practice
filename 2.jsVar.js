7f82cc63b31824342e468c55c98a56ad05631fc6
Ralph <justinefar00@gmail.com>
2025-07-06 23:26:09 +0700

    Basic Variable

---
+++ 2.jsVar.js
@@ -0,0 +1,29 @@
+//Using var, let, and const variable
+
+/*Var Variable
+var userName = "Kukre00"
+console.log(`The username is ${userName}`)
+
+Update
+userName = "Kuks"
+console.log(`The new username is ${userName}`)
+*/
+
+
+/*Let Variable
+let petName = "Hoshi"
+console.log(`My pet's name, ${petName}`)
+
+//Update
+petName = "Luna"
+console.log(`My pet's name, ${petName}`)
+*/
+
+//The var and let are similar, they can update or reassign a data. The let variable is new version of var variable
+
+//Const Variable
+const userId = "09-10-0001"
+console.log(`User ID: ${userId}`)
+
+//in const variable, the data you put are not updatable or it cannot reassign a data. 
+
